4a0f6b4648e8c9dc141c81013457964236aa0447
Savina Daria <savina.dariia@chnu.edu.ua>
2024-06-26 18:44:05 +0300

    jpa entity

---
+++ src/main/java/com/example/demo/entities/JeweleryItem.java
@@ -0,0 +1,69 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "jeweleryItems")
+public class JeweleryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType. IDENTITY)
+    private int id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "material", nullable = false)
+    private String material;
+    @Column(name = "cost", nullable = false)
+    private double cost;
+
+    public JeweleryItem(int id, String name, String type, String material, double cost) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.material = material;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getmaterial() {
+        return material;
+    }
+
+    public void setmaterial(String material) {
+        material = material;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
