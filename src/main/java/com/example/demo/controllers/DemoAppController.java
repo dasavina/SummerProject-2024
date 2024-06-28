dbd7358d90f215126bb78f1e20554c07ccf3e342
Savina Daria <savina.dariia@chnu.edu.ua>
2024-06-28 17:42:08 +0300

    delete message

--- src/main/java/com/example/demo/controllers/DemoAppController.java
+++ src/main/java/com/example/demo/controllers/DemoAppController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -62,8 +63,9 @@ public class DemoAppController {
     }
 
     @GetMapping("/items/{id}")
-    public String deleteItem(@PathVariable Integer id) {
+    public String deleteItem(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
         service.deleteItemById(id);
+        redirectAttributes.addFlashAttribute("successMessage", "Successfully deleted!");
         return "redirect:/items";
     }
 }
